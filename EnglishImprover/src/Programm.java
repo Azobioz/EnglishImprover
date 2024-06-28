a7ba4b306c2c8e52571ebbdeb0cda6bf0f436b21
Azobioz <145475512+Azobioz@users.noreply.github.com>
2024-06-28 21:25:23 +0300

    add commit

--- EnglishImprover/src/Programm.java
+++ EnglishImprover/src/Programm.java
@@ -1,12 +1,14 @@
 import StartTraining.*;
 
 import java.io.Console;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 
 public class Programm {
     public static void main(String[] args) {
-        FileVocabulary.startWorkWithFile();
-
+        //FileVocabulary.startWorkWithFile();
+        
     }
 }
