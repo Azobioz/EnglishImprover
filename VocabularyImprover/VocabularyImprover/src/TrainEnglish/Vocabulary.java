789083f0f14755621967ad739d0af589299af2f8
Azobioz <145475512+Azobioz@users.noreply.github.com>
2024-07-17 05:00:44 +0300

    Add commit
    Changed names

---
+++ VocabularyImprover/VocabularyImprover/src/TrainEnglish/Vocabulary.java
@@ -0,0 +1,9 @@
+package TrainEnglish;
+
+public interface Vocabulary {
+   String findWord(String word);
+   String findWordFromIndex(int index);
+   void showWords();
+   boolean deleteWord(String word);
+   boolean writeIntoStorage(String word, String... translation);
+}
