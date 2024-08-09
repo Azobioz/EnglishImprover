d6a4c3e33974f69063c53c74aca2ba653332dbff
Azobioz <145475512+Azobioz@users.noreply.github.com>
2024-08-09 18:11:56 +0300

    Delete DBForVocabulary directory

--- DBForVocabulary/Insert Words.sql
+++
@@ -1,6 +0,0 @@
-call words_insert('Apple', 'Яблоко');
-call words_insert('Car', 'Кошка');
-call words_insert('Piano', 'Пианино');
-call words_insert('Ear', 'Ухо');
-call words_delete(1);
-select * from words;
